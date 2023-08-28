b3e47a836f445afbdbb1c6d5db8e96b61e37bde7
juanprojects <136933404+juanprojects@users.noreply.github.com>
2023-08-27 20:23:09 -0500

    send
    asdasd

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+38r*n!@&cn+#$lp(fxqbwzw*le0^(a%h@cokjj2adj*n4hi6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

219f7f64a8325792fd160385aa15946cbc681956
juanprojects <136933404+juanprojects@users.noreply.github.com>
2023-08-28 10:03:33 -0500

    nuevo intento

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -62,7 +62,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            BASE_DIR / 'frontend/build',
+	    BASE_DIR / 'templates'	
         ],
         'APP_DIRS': True,
         'OPTIONS': {
